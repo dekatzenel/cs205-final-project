d98572e4358ec8f89d0a357a17c403475ed90e1c
Dana Katzenelson <dkatzenelson@college.harvard.edu>
2015-11-11 21:57:41 -0600

    simulated annealing function complete

--- annealing_helper_functions.py
+++ annealing_helper_functions.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+# Functions below adapted from ac290_tutorial2_simulated_annealing-student.ipynb
+
 #Assumes citypath is a list/1D array of city numeric indices
 def distance(graph, citypath):
     distance = 0
@@ -13,8 +15,6 @@ def distance(graph, citypath):
             distance += graph[citypath[j], citypath[j+1]]
     return distance
 
-# Functions below taken from ac290_tutorial2_simulated_annealing-student.ipynb without change
-
 #Helper function for changepath
 def swapindex(cities):
     indices = range(len(cities))
@@ -22,13 +22,13 @@ def swapindex(cities):
     c1,c2 = np.random.choice(indices, 2, replace=False)
     
     # remember the cities to swap
-    tmp1 = cities[c1,:]
-    tmp2 = cities[c2,:]
+    tmp1 = cities[c1]
+    tmp2 = cities[c2]
     
     # do the actual swapping
     changedCities = cities.copy()
-    changedCities[c1,:] = tmp2
-    changedCities[c2,:] = tmp1    
+    changedCities[c1] = tmp2
+    changedCities[c2] = tmp1    
     return changedCities
 
 #Clever updating function for simulated annealing
@@ -39,3 +39,40 @@ def changepath(inputcities, n_swaps):
         swappedCities = swapindex(cities)
         cities=swappedCities.copy()
     return cities
+
+def simulated_annealing(graph, function, initial_X, initial_temp, cool, reanneal, iterr, swap_function, nswaps):
+    
+    accepted = 0
+    X = initial_X.copy()
+    T = initial_temp
+    
+    history = list()
+    # Evaluate E
+    prev_E = function(graph, X)
+    history.append(prev_E)
+    
+    for i in xrange(iterr):
+        # Stepsize
+        L = np.sqrt(T)
+        # Randomly update path
+        X_star = swap_function(X, nswaps)
+        # Evaluate E
+        new_E = function(graph, X_star)
+        delta_E = new_E - prev_E
+        
+        # Flip a coin
+        U = np.random.uniform()
+        if U < np.exp(-delta_E / T):
+            accepted += 1
+            history.append(new_E)
+            # Copy X_star to X
+            X = X_star.copy()
+            prev_E = new_E
+
+        # Check to cool down
+        if accepted % reanneal == 0:
+            T *= cool
+            if T < 0.001: # Reheat
+                T = 1.
+            
+    return X, history
--- controller.py
+++ controller.py
@@ -1,9 +1,26 @@
 import numpy as np
 
-from annealing_helper_functions import distance
+from annealing_helper_functions import distance, changepath, simulated_annealing
 from xml_parse import parse_xml_graph
 
 graph = parse_xml_graph('fri26.xml')
+#Square matrix
+assert graph.shape[0] == graph.shape[1]
+size = graph.shape[0]
+
 # Hardcoded best path to validate distance calculations, zero-indexed
-bestpath = [x-1 for x in [1, 25, 24, 23, 26, 22, 21, 17, 18, 20, 19, 16, 11, 12, 13, 15, 14, 10, 9, 8, 7, 5, 6, 4, 3, 2]]
-print distance(graph, bestpath)
+bestpath = np.asarray([x-1 for x in [1, 25, 24, 23, 26, 22, 21, 17, 18, 20, 19, 16, 11, 12, 13, 15, 14, 10, 9, 8, 7, 5, 6, 4, 3, 2]])
+print "Best path: " + str(bestpath)
+print "Best path length: " + str(distance(graph, bestpath))
+
+#Initial values, probably need to be tuned
+initial_path = np.random.permutation(size)
+initial_temp = 2.
+cool = 0.9
+reanneal = 100
+iterr = 100000
+nswaps = 3
+
+solution, history = simulated_annealing(graph, distance, initial_path, initial_temp, cool, reanneal, iterr, changepath, nswaps)
+print "Calculated path: " + str(solution)
+print "Calculated path length: " + str(distance(graph, solution))
