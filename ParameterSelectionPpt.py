521b007da09ad5057ffcd9e46a89e6bab828f838
ChristianJunge <christianjunge@g.harvard.edu>
2015-12-10 14:54:59 -0500

    Merged code and got rid of version conflicts

--- ParameterSelectionPpt.py
+++ ParameterSelectionPpt.py
@@ -1,6 +1,6 @@
 import numpy as np
-from annealing_helper_functions import distance, changepath, simulated_annealing, serial_parallel_tempering
-from parallelism import parallel_parallel_tempering
+from annealing_helper_functions import distance, changepath, simulated_annealing
+from parallel_tempering import parallel_parallel_tempering, serial_parallel_tempering
 from timer import Timer
 from xml_parse import parse_xml_graph
 from multiprocessing import freeze_support
